09aa3e7933fa8856fb67e41f65f0e4217e84ce2d
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2025-05-24 21:28:54 +0300

    Delete solutions/1744. Can You Eat Your Favorite Candy on Your Favorite Day? directory

--- solutions/1744. Can You Eat Your Favorite Candy on Your Favorite Day?/1744.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-  def canEat(
-      self,
-      candiesCount: list[int],
-      queries: list[list[int]]
-  ) -> list[bool]:
-    prefix = list(itertools.accumulate(candiesCount, initial=0))
-    return [prefix[t] // c <= d < prefix[t + 1] for t, d, c in queries]
